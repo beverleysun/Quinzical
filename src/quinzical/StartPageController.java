461518c6ab5fe9777d85a79eb27fb183b6890ffa
beverleysun <beverleysun0205@gmail.com>
2020-09-19 21:29:49 +1200

    Implement quit button

---
+++ src/quinzical/StartPageController.java
@@ -0,0 +1,28 @@
+package quinzical;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartPageController {
+
+    public void quit(ActionEvent e) throws IOException {
+        // Go to quit scene
+        Parent quit = FXMLLoader.load(getClass().getResource("QuitPrompt.fxml"));
+
+        // Set scene width and height to the previous scene width and height
+        Scene prevScene = ((Node)e.getSource()).getScene();
+        Stage window = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        double prevSceneWidth = prevScene.getWidth();
+        double prevSceneHeight = prevScene.getHeight();
+        Scene quitScene = new Scene(quit, prevSceneWidth, prevSceneHeight);
+
+        // Show scene
+        window.setScene(quitScene);
+    }
+}
