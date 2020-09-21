a60fdd710aaba8e60df1b9335dae5fef22302b60
Beverley Sun <beverleysun0205@gmail.com>
2020-09-21 13:53:42 +1200

    Remove unused imports

--- src/quinzical/StartPageController.java
+++ src/quinzical/StartPageController.java
@@ -2,10 +2,7 @@ package quinzical;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
