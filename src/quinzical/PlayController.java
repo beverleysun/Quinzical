d4990388b91348c9c2d7ead9cc1c453925fa03d4
Beverley Sun <beverleysun0205@gmail.com>
2020-09-21 21:39:28 +1200

    Initialise question buttons

--- src/quinzical/PlayController.java
+++ src/quinzical/PlayController.java
@@ -3,35 +3,37 @@ package quinzical;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PlayController {
 
     @FXML
-    private Label cat1Label;
+    private Label cat1Label, cat2Label, cat3Label, cat4Label, cat5Label;
 
     @FXML
-    private Label cat2Label;
+    private Button cat1value100Button, cat1value200Button, cat1value300Button, cat1value400Button, cat1value500Button,
+                    cat2value100Button, cat2value200Button, cat2value300Button, cat2value400Button, cat2value500Button,
+                    cat3value100Button, cat3value200Button, cat3value300Button, cat3value400Button, cat3value500Button,
+                    cat4value100Button, cat4value200Button, cat4value300Button, cat4value400Button, cat4value500Button,
+                    cat5value100Button, cat5value200Button, cat5value300Button, cat5value400Button, cat5value500Button ;
 
-    @FXML
-    private Label cat3Label;
 
-    @FXML
-    private Label cat4Label;
-
-    @FXML
-    private Label cat5Label;
 
     private Database _database = Database.getInstance();
     private List<Category> _questionData = _database.getQuestionData();
+    private List<Button> _buttons = new ArrayList<Button>();
 
     @FXML
     public void initialize() {
         setLabels();
+        initButtons();
     }
 
     public void setLabels() {
@@ -40,6 +42,26 @@ public class PlayController {
         cat3Label.setText(_questionData.get(2).getCategoryName());
         cat4Label.setText(_questionData.get(3).getCategoryName());
         cat5Label.setText(_questionData.get(4).getCategoryName());
+    }
+
+    public void initButtons() {
+        Collections.addAll(_buttons, cat1value100Button, cat1value200Button, cat1value300Button, cat1value400Button, cat1value500Button,
+                cat2value100Button, cat2value200Button, cat2value300Button, cat2value400Button, cat2value500Button,
+                cat3value100Button, cat3value200Button, cat3value300Button, cat3value400Button, cat3value500Button,
+                cat4value100Button, cat4value200Button, cat4value300Button, cat4value400Button, cat4value500Button,
+                cat5value100Button, cat5value200Button, cat5value300Button, cat5value400Button, cat5value500Button);
+        for (Button button: _buttons) {
+            int category = Integer.parseInt(button.getId().split("-")[0]);
+            int value = Integer.parseInt(button.getId().split("-")[1]);
+
+            Question questionToAsk = _questionData.get(category-1).findQuestionByValue(value);
+
+            if (!questionToAsk.isAvailable()) {
+                button.setDisable(true);
+            } else {
+                button.setDisable(false);
+            }
+        }
     }
 
     public void back(MouseEvent e) {
