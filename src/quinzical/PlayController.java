ca7bf514f7ddf9ef05a0375427b06a74b8c9acec
Beverley Sun <beverleysun0205@gmail.com>
2020-09-21 13:52:33 +1200

    Go back to start page on click of arrow

--- src/quinzical/PlayController.java
+++ src/quinzical/PlayController.java
@@ -1,4 +1,19 @@
 package quinzical;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class PlayController {
+    public void back (MouseEvent e) {
+        try {
+            // Load start page scene
+            Parent startPage = FXMLLoader.load(getClass().getResource("StartPage.fxml"));
+            SceneChanger.changeScene(e, startPage);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
 }
