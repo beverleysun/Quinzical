f2b5050224bfc4efc35e77c3b03f8f8cd26238ed
Beverley Sun <beverleysun0205@gmail.com>
2020-09-23 20:39:23 +1200

    Custom constructor for this controller

---
+++ src/quinzical/AskQuestionController.java
@@ -0,0 +1,14 @@
+package quinzical;
+
+public class AskQuestionController {
+
+    private String _questionStr;
+    private String _answerStr;
+
+    public AskQuestionController(String questionStr, String answerStr) {
+        _questionStr = questionStr;
+        _answerStr = answerStr;
+    }
+
+
+}
