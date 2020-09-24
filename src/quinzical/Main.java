290cb011b83cca45d60b4bcac2688497f16a68ae
Beverley Sun <beverleysun0205@gmail.com>
2020-09-24 23:03:32 +1200

    Load custom font

--- src/quinzical/Main.java
+++ src/quinzical/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("StartPage.fxml"));
+        Font.loadFont(getClass().getResourceAsStream("../assets/PTSans-Regular"), 14);
         primaryStage.setTitle("Quinzical");
 
         primaryStage.setMinHeight(300);
