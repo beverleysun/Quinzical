87de61582fdc99bc438dec000bb3eaa40529a243
Beverley Sun <beverleysun0205@gmail.com>
2020-09-24 17:58:16 +1200

    Set the question text when scene loads

--- src/quinzical/AskQuestionController.java
+++ src/quinzical/AskQuestionController.java
@@ -1,13 +1,28 @@
 package quinzical;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class AskQuestionController {
 
     private String _questionStr;
     private String _answerStr;
+    private String _categoryStr;
+    private int _value;
 
-    public AskQuestionController(String questionStr, String answerStr) {
+    @FXML
+    private Label questionInfo;
+
+    public AskQuestionController(String questionStr, String answerStr, String categoryStr, int value) {
         _questionStr = questionStr;
         _answerStr = answerStr;
+        _categoryStr = categoryStr;
+        _value = value;
+    }
+
+    @FXML
+    public void initialize() {
+        questionInfo.setText("Playing " + _categoryStr + " for $" + _value);
     }
 
 
