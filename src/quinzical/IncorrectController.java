17c058cac41648aee473006cd7928cc998e813fc
Beverley Sun <beverleysun0205@gmail.com>
2020-09-25 15:02:12 +1200

    Control elements on incorrect scene

---
+++ src/quinzical/IncorrectController.java
@@ -0,0 +1,33 @@
+package quinzical;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class IncorrectController extends ConfirmController {
+
+    @FXML
+    private Label answerLabel;
+
+    @FXML
+    private Button nextButton;
+
+    private String _answer;
+
+    public IncorrectController(String answer) {
+        super();
+        _answer =  answer;
+    }
+
+    @FXML
+    public void initialize() {
+        answerLabel.setText("The correct answer was " + _answer);
+        TTS.getInstance().speak("Oops. The correct answer was " + _answer);
+        nextButton.setOnMouseClicked(this::toQuestionBoard);
+    }
+
+    public void toQuestionBoard(MouseEvent e) {
+        super.toQuestionBoard(e);
+    }
+}
