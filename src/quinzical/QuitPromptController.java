450247ad58576cc34ed0d18352532985355cf54d
Beverley Sun <beverleysun0205@gmail.com>
2020-09-25 18:02:54 +1200

    Change to MouseEvent instead of ActionEvent

--- src/quinzical/QuitPromptController.java
+++ src/quinzical/QuitPromptController.java
@@ -1,22 +1,22 @@
 package quinzical;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class QuitPromptController {
 
-    public void noQuit(ActionEvent e) throws IOException {
+    public void noQuit(MouseEvent e) throws IOException {
         // Go back to start page
         Parent quit = FXMLLoader.load(getClass().getResource("StartPage.fxml"));
         SceneChanger.changeScene(e, quit);
     }
 
-    public void yesQuit(ActionEvent e) {
+    public void yesQuit(MouseEvent e) {
         // Close the game
         Stage window = (Stage) ((Node)e.getSource()).getScene().getWindow();
         window.close();
