7008a9b8cbcab6eacf6afd3e386a341d0820a784
Beverley Sun <beverleysun0205@gmail.com>
2020-09-25 15:00:57 +1200

    Validate answer given

--- src/quinzical/AskQuestionController.java
+++ src/quinzical/AskQuestionController.java
@@ -1,8 +1,15 @@
 package quinzical;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class AskQuestionController {
 
@@ -17,11 +24,18 @@ public class AskQuestionController {
     @FXML
     private Slider voiceSlider;
 
+    @FXML
+    private Button confirm, giveUp;
+
+    @FXML
+    private TextField textField;
+
     public AskQuestionController(String questionStr, String answerStr, String categoryStr, int value) {
         _questionStr = questionStr;
         _answerStr = answerStr;
         _categoryStr = categoryStr;
         _value = value;
+
     }
 
     @FXML
@@ -30,5 +44,48 @@ public class AskQuestionController {
         voiceSlider.setValue(TTS.getInstance().getMultiplier());
         winnings.setText("$" + Database.getInstance().getWinnings());
         TTS.getInstance().speak(_questionStr);
+
+        confirm.setOnMouseClicked(this::confirm);
+        giveUp.setOnMouseClicked(this::loadIncorrectScene);
+
     }
+
+    public void replay() {
+        TTS.getInstance().speak(_questionStr);
+    }
+
+    public void confirm(MouseEvent e) {
+        String userAnswer = textField.getText();
+        if (_answerStr.equals(userAnswer)) {
+            Database.getInstance().addWinnings(_value);
+            loadCorrectScene(e);
+        } else {
+            loadIncorrectScene(e);
+        }
+    }
+
+    public void loadIncorrectScene(MouseEvent e) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Incorrect.fxml"));
+        loader.setController(new IncorrectController(_answerStr));
+        try {
+            Parent incorrect = loader.load();
+            SceneChanger.changeScene(e, incorrect);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public void loadCorrectScene(MouseEvent e){
+        try {
+            Parent correct = FXMLLoader.load(getClass().getResource("Correct.fxml"));
+            SceneChanger.changeScene(e, correct);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public void sliderChanged() {
+        TTS.getInstance().setMultiplier(voiceSlider.getValue());
+    }
+
 }
