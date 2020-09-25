26eca7fe290ef9fb7680bfb99417b78b5dd7eafd
Beverley Sun <beverleysun0205@gmail.com>
2020-09-25 15:01:31 +1200

    Abstract class to confirm if answer was correct

---
+++ src/quinzical/ConfirmController.java
@@ -0,0 +1,37 @@
+package quinzical;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+public abstract class ConfirmController {
+    @FXML
+    private Label winnings;
+
+    @FXML
+    private Slider voiceSlider;
+
+    @FXML
+    public void initialize() {
+        winnings.setText("$" + Database.getInstance().getWinnings());
+        voiceSlider.setValue(TTS.getInstance().getMultiplier());
+    }
+
+    public void toQuestionBoard(MouseEvent e){
+        try {
+            Parent questionBoard = FXMLLoader.load(getClass().getResource("Play.fxml"));
+            SceneChanger.changeScene(e, questionBoard);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    public void sliderChanged() {
+        TTS.getInstance().setMultiplier(voiceSlider.getValue());
+    }
+}
