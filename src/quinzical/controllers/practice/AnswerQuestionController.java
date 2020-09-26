af954e1027f37add3f6f157e0e8f34b06e3614e9
JinkaiZ <Jzha541@aucklanduni.ac.nz>
2020-09-26 23:57:26 +1200

    Change the slider in the practice module

--- src/quinzical/controllers/practice/AnswerQuestionController.java
+++ src/quinzical/controllers/practice/AnswerQuestionController.java
@@ -31,7 +31,6 @@ public class AnswerQuestionController extends PlayController {
     public void initialize(){
         questionClue.setText(PracticeController.getClue());
         questionClue.setWrapText(true);
-
         _unattemptedTime = 4 - PracticeController.getQuestion().getAnsweredTimes();
         System.out.print(_unattemptedTime);
         if(_unattemptedTime > 1) {
