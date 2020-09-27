58bc1c84025e01ce90a8ac59c9a56cad1faf39b8
Beverley Sun <beverleysun0205@gmail.com>
2020-09-28 12:31:42 +1300

    Refactor

--- src/quinzical/controllers/practice/AnswerQuestionController.java
+++ src/quinzical/controllers/practice/AnswerQuestionController.java
@@ -4,24 +4,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import quinzical.SceneChanger;
 import quinzical.TTS;
+import quinzical.controllers.VoiceSpeedChangeable;
 
 import java.io.File;
 import java.io.IOException;
 
-public class AnswerQuestionController extends PracticeController {
+public class AnswerQuestionController extends VoiceSpeedChangeable {
 
-
-    
-	private  String _realAnswer;
-    private  String _givenAnswer;
     private static boolean _result;
     private static int _unattemptedTime;
-
 
     @FXML
     private Label questionClue;
@@ -29,32 +24,28 @@ public class AnswerQuestionController extends PracticeController {
     private Label hintLabel;
     @FXML
     private TextField answerInput;
-    
-    @FXML
-    private Slider voiceSlider;
+
 
     @FXML
     public void initialize(){
-    	voiceSlider.setValue(TTS.getInstance().getMultiplier());
+        super.initialize();
         questionClue.setText(PracticeController.getClue());
         TTS.getInstance().speak(PracticeController.getClue());
-        questionClue.setWrapText(true);
         _unattemptedTime = 4 - PracticeController.getQuestion().getAnsweredTimes();
         System.out.print(_unattemptedTime);
         if(_unattemptedTime > 1) {
             hintLabel.setText("You have " + _unattemptedTime + " chances to answer the question from " + PracticeController.getCategory());
         }
         else{
-            hintLabel.setText("This is your last chance to answer the question. Here is the hint, " + PracticeController.getHint() + " " + PracticeController.getAnswerFirstLetter() + "......??");
+            hintLabel.setText("This is your last chance. Hint, " + PracticeController.getHint() + " " + PracticeController.getAnswerFirstLetter() + "...");
         }
     }
 
     @FXML
     private void checkAnswer(MouseEvent e) {
-        _realAnswer = answerInput.getText();
-        _givenAnswer = PracticeController.getAnswer();
+        String _realAnswer = answerInput.getText();
+        String _givenAnswer = PracticeController.getAnswer();
         try {
-
             if (_realAnswer.toLowerCase().trim().equals(_givenAnswer.toLowerCase().trim())) {
                 _result = true;
                 Parent answer = FXMLLoader.load(getClass().getResource("../../scenes/practice/Answer.fxml"));
@@ -73,16 +64,12 @@ public class AnswerQuestionController extends PracticeController {
 
     }
 
-    
-
     @FXML
     public void giveUp(MouseEvent e) throws IOException {
         skipQuestion(e);
     }
 
-
     public static void skipQuestion(MouseEvent e) throws IOException {
-        
         Parent answer = FXMLLoader.load(AnswerQuestionController.class.getResource("../../scenes/practice/Practice.fxml"));
         SceneChanger.changeScene(e, answer);
         PracticeController.getDatabase().getPracticeQuestionData().clear();
@@ -94,6 +81,11 @@ public class AnswerQuestionController extends PracticeController {
         }
 
         PracticeController.getDatabase().loadPracticeQuestions();
+    }
+
+    @FXML
+    public void replay() {
+        TTS.getInstance().speak(PracticeController.getClue());
     }
 
     public static boolean getResult(){
