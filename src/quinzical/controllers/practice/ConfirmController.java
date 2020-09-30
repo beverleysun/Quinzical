76e10f59dc599636d39f076869849f2cee1914d0
Beverley Sun <beverleysun0205@gmail.com>
2020-09-30 21:23:37 +1300

    Implement VoiceSpeedChangeable to avoid code duplication

---
+++ src/quinzical/controllers/practice/ConfirmController.java
@@ -0,0 +1,27 @@
+package quinzical.controllers.practice;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import quinzical.SceneChanger;
+import quinzical.controllers.VoiceSpeedChangeable;
+
+import java.io.IOException;
+
+public abstract class ConfirmController extends VoiceSpeedChangeable {
+
+    @FXML
+    public void initialize() {
+        super.initialize();
+    }
+
+    public void backToPractice(MouseEvent e) {
+        try {
+            Parent practice = FXMLLoader.load(AnswerQuestionController.class.getResource("../../scenes/practice/Practice.fxml"));
+            SceneChanger.changeScene(e, practice);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
