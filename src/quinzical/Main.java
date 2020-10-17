217e205d21e2cbc85799d9b63cdf6f9eb68e76ac
Beverley Sun <beverleysun0205@gmail.com>
2020-10-17 21:45:33 +1300

    Try-catch

--- src/quinzical/Main.java
+++ src/quinzical/Main.java
@@ -7,19 +7,25 @@ import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("scenes/StartPage.fxml"));
-        Font.loadFont(getClass().getResourceAsStream("../assets/PTSans-Regular"), 14);
-        primaryStage.setTitle("Quinzical");
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("scenes/StartPage.fxml"));
+            Font.loadFont(getClass().getResourceAsStream("../assets/PTSans-Regular"), 14);
 
-        primaryStage.setMinHeight(300);
-        primaryStage.setMinWidth(400);
+            primaryStage.setTitle("Quinzical");
+            primaryStage.setMinHeight(300);
+            primaryStage.setMinWidth(400);
 
-        primaryStage.setScene(new Scene(root, 800, 500));
-        primaryStage.show();
+            primaryStage.setScene(new Scene(root, 800, 500));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
